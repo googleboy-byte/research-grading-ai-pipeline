171cc985f9513052241eccc2cf746663d66cd1f0
Mainak Dasgupta <69593396+googleboy-byte@users.noreply.github.com>
2025-01-13 15:04:38 +0530

    Delete master directory

--- master/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .feature_extractor import extract_features
-from .classifier import ConferenceRecommender, PublishabilityClassifier
-
-__all__ = ['extract_features', 'ConferenceRecommender', 'PublishabilityClassifier'] 
